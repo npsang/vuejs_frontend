bcdb994eaf55b4f8b5907e963ce9faceb211a24d
npsang <npsangservices@gmail.com>
2023-01-17 23:21:44 +0700

    change localhost backend link, change sigup and login with OAuth2-Django

--- vue-3-authentication-jwt/src/services/auth.service.js
+++ vue-3-authentication-jwt/src/services/auth.service.js
@@ -1,16 +1,21 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/auth/';
+
+
+const API_URL = 'http://127.0.0.1:8000/';
 
 class AuthService {
   login(user) {
     return axios
-      .post(API_URL + 'signin', {
+      .post(API_URL + 'o/token/', {
+        client_id: 'KziZ3obenm05pj16kfvERM0iaXQazBUY9OFsByYc',
+        client_secret: 'ywVoT2piM7QK9Mk7MmVJn6qNHYLwhVduY38BIO5eUw8bYii7d4wMeAnEGyV2dMF1VvXiI4R2WpVvTZGTv5RqzfcApR3oIPiIiHfj1nAXD6jwTYQkpSHOL65pf0w3cIkc',
+        grant_type: 'password',
         username: user.username,
         password: user.password
       })
       .then(response => {
-        if (response.data.accessToken) {
+        if (response.data.access_token) {
           localStorage.setItem('user', JSON.stringify(response.data));
         }
 
@@ -23,12 +28,16 @@ class AuthService {
   }
 
   register(user) {
-    return axios.post(API_URL + 'signup', {
+    return axios.post(API_URL + 'users/', {
       username: user.username,
       email: user.email,
-      password: user.password
+      password: user.password, 
+      documents: [],
     });
   }
 }
 
 export default new AuthService();
+// {headers: {"Access-Control-Allow-Origin": "*",
+//     "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+//     "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token"}
